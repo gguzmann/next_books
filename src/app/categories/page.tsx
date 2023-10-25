0df8469ee1cd1d4d0894fcf3cea6e32c0901d66b
gguzman <gguzman@bice.cl>
2023-10-25 09:45:45 -0300

    db

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,11 +1,7 @@
-import { fetchData } from '@/data/getData'
-
 export default async function Categories () {
-  const books = await fetchData()
   return (
     <>
     <h1>Categorias</h1>
-    {books.length}
     </>
   )
 }

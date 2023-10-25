ebc4f3c9d3c1cd94c5549be6aea91ae58e1bf02f
gguzman <gguzman@bice.cl>
2023-10-25 09:53:19 -0300

    db

--- src/data/getData.ts
+++ src/data/getData.ts
@@ -1,5 +1,5 @@
 export const fetchData = async () => {
-  const res = await fetch(process.env.HOST + '/api/books')
+  const res = await fetch('https://next-books-theta.vercel.app/api/books')
   const { books } = await res.json()
   console.log(process.env.HOST)
   return books

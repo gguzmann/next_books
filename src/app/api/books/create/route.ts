388820141ae877064a47c5ccca243fe907a53243
gguzman <gguzman@bice.cl>
2023-10-25 11:02:40 -0300

    add data

--- src/app/api/books/create/route.ts
+++ src/app/api/books/create/route.ts
@@ -1,18 +1,8 @@
-import { books, booksTest } from '@/data/data'
+import { books } from '@/data/data'
 import prisma from '@/data/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET () {
-  const obj: any = {
-    title: 'Unlocking Android',
-    isbn: '1933988673',
-    publishedDate: new Date('2009-04-01T00:00:00.000-0700'),
-    thumbnailUrl: 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg',
-    status: 'PUBLISH',
-    authors: ['W. Frank Ableson', 'Charlie Collins', 'Robi Sen'],
-    categories: ['Open Source', 'Mobile']
-  }
-
   const arrayBook = books.map(book => {
     return {
       title: book.title ?? '',

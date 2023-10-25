4587d9846c5dfb8908bc85eec54248795c410040
gguzman <gguzman@bice.cl>
2023-10-25 11:04:21 -0300

    fix categories

--- src/data/getData.ts
+++ src/data/getData.ts
@@ -3,7 +3,5 @@ export const dynamic = 'force-dynamic' // If you want no caching at all
 export const fetchData = async () => {
   const res = await fetch('https://next-books-theta.vercel.app/', { next: { revalidate: 1 } })
   const { books } = await res.json()
-  console.log(process.env.HOST)
-  console.log(books)
   return books
 }

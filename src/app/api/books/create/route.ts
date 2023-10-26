37405b9d7ac92f30fcbc4e757341e8a522114a8d
gguzman <gguzman@bice.cl>
2023-10-26 11:21:41 -0300

    fix

--- src/app/api/books/create/route.ts
+++ src/app/api/books/create/route.ts
@@ -1,24 +1,26 @@
-import { books } from '@/data/data'
-import prisma from '@/data/prisma'
+// import { books } from '@/data/data'
+// import prisma from '@/data/prisma'
+// import type { Book } from '@/model/interfaces'
 import { NextResponse } from 'next/server'
 
 export async function GET () {
-  const arrayBook = books.map(book => {
-    return {
-      title: book.title ?? '',
-      isbn: book.isbn ?? '',
-      pageCount: book.pageCount ?? '0',
-      publishedDate: new Date('2009-04-01T00:00:00.000-0700') ?? '',
-      thumbnailUrl: book.thumbnailUrl ?? '',
-      shortDescription: book.shortDescription ?? '',
-      longDescription: book.longDescription ?? '',
-      status: book.status ?? '',
-      authors: book.authors ?? '',
-      categories: book.categories
-    }
-  })
-  const test = await prisma.book.createMany({ data: arrayBook })
-  console.log(test)
+  // const arrayBook: Book[] = books.map(book => {
+  //   return {
+  //     id: 0,
+  //     title: book.title ?? '',
+  //     isbn: book.isbn ?? '',
+  //     pageCount: book.pageCount ?? 0,
+  //     publishedDate: new Date('2009-04-01T00:00:00.000-0700') ?? '',
+  //     thumbnailUrl: book.thumbnailUrl ?? '',
+  //     shortDescription: book.shortDescription ?? '',
+  //     longDescription: book.longDescription ?? '',
+  //     status: book.status ?? '',
+  //     authors: book.authors ?? '',
+  //     categories: book.categories
+  //   }
+  // })
+  // const test = await prisma.book.createMany({ data: arrayBook })
+  // console.log(test)
   return NextResponse.json({
     message: 'create data books'
   })
